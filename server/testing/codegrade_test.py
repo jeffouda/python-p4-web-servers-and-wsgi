e9c619433e223288d0e1e94276dd5b1c378689ea
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-12-17 00:47:24 +0300

    Implement Werkzeug WSGI app and add test

--- server/testing/codegrade_test.py
+++ server/testing/codegrade_test.py
@@ -1,4 +1,9 @@
+from werkzeug.test import Client
+from server.werkzeug_app import application
 
-def test_codegrade_placeholder():
-    """Codegrade placeholder test"""
-    assert 1==1
+
+def test_wsgi_response():
+    client = Client(application)
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"A WSGI generated this response!" in response.data
